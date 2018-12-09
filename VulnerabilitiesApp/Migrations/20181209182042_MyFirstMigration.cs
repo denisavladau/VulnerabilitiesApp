43c4af03aa662b914d7bdf7fcb22c38d86176f74
PC1 <PC1@DESKTOP-CELRRBC>
2018-12-09 20:27:44 +0200

    Creare Pagina Utilizatori + Migrari

---
+++ VulnerabilitiesApp/Migrations/20181209182042_MyFirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VulnerabilitiesApp.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utilizator",
+                columns: table => new
+                {
+                    Sters = table.Column<bool>(nullable: false),
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nume = table.Column<string>(nullable: true),
+                    Prenume = table.Column<string>(nullable: true),
+                    Telefon = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilizator", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Utilizator");
+        }
+    }
+}
